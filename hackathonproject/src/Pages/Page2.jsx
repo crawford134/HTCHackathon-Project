f3c7d6ee4d23739b7e9c054fd911fa23be9046dc
Stam743 <spencertam@rocketmail.com>
2021-11-14 09:27:44 -0700

    attempting to fix

--- hackathonproject/src/Pages/Page2.jsx
+++ hackathonproject/src/Pages/Page2.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Resources from '../Components/Resources';
+import SurveyAnswers from '../Data/SurveyAnswers';
 
 function Page2() {
   return (
     <div className="App">
       <header className="App-header">
         <Resources />
+        {console.log("Page2:", SurveyAnswers)}
       </header>
     </div>
   );
